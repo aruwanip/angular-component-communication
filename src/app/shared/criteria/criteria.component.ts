9e2542f98f975e627cc430e242c1f777a78175b1
Pinkie Aruwani <aruwanip@gmail.com>
2021-12-22 13:31:46 -0500

    Using OnChanges lifecycle hook in child component to react to changes in Input variable

--- src/app/shared/criteria/criteria.component.ts
+++ src/app/shared/criteria/criteria.component.ts
@@ -1,16 +1,26 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
 
   @Input() displayDetail: boolean;
   @Input() hitCount;
 
   listFilter: string;
+  hitMessage: string;
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
   constructor() {
@@ -21,6 +31,14 @@ export class CriteriaComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.filterElementRef.nativeElement.focus();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = 'No matches found';
+    } else {
+      this.hitMessage = 'Hits: ' + this.hitCount;
+    }
   }
 
 }
