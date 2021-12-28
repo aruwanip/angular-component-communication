231c75756434b792c3a552003982230eaab3a987
Pinkie Aruwani <aruwanip@gmail.com>
2021-12-28 18:28:22 -0500

    Implementing second subscriber to selected product to demonstrate Subject's multicasting

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
     templateUrl: './product-shell.component.html'
 })
@@ -7,9 +9,19 @@ export class ProductShellComponent implements OnInit {
     pageTitle: string = 'Products';
     monthCount: number;
 
-    constructor() { }
+    constructor(private productService: ProductService) {
+    }
 
     ngOnInit() {
+        this.productService.selectedProductChanges$.subscribe(selectedProduct => {
+            if (selectedProduct) {
+                const start = new Date(selectedProduct.releaseDate);
+                const now = new Date();
+                this.monthCount = now.getMonth() - start.getMonth() + (12 * (now.getFullYear() - start.getFullYear()));
+            } else {
+                this.monthCount = 0;
+            }
+        });
     }
 
 }
